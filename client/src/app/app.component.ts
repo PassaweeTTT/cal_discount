6a9ddad7b156acf0225379591d52556fe15c1100
PassaweeTTT <wan.passawee@gmail.com>
2025-04-25 14:50:25 +0700

    install ngx-bootstrap

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component, TemplateRef } from '@angular/core';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [CarouselModule, TooltipModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    providers: [BsModalService]
 })
 export class AppComponent {
-  title = 'client';
+    title = 'client';
+
+    constructor(private modalService: BsModalService) { }
+
+    modalRef?: BsModalRef;
+    openModal(template: TemplateRef<void>) {
+        this.modalRef = this.modalService.show(template);
+    }
 }
